8611aaf54076a1260ca4c32594b914491b026fa8
Made Indrayana <indra@doubleshot-audio.com>
2021-07-03 12:08:22 +0200

    Basic function and !cls command :D

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("../config.json");
+
+const prefix = config.prefix;
+
+async function clearAllMessagesByCloning(channel) {
+
+    channel.send("Trying to delete...");
+
+    // Clone channel
+    const newChannel = await channel.clone()
+
+    // Delete old channel
+    channel.delete()
+}
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  else if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+    
+  } 
+  else if (message.content.startsWith(prefix + "cls")) {
+    clearAllMessagesByCloning(message.channel);
+  }
+});
+
+client.login(config.token);
