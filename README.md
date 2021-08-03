f9e79af8820a3d2f8ea31993dc468048db85fcc9
Made Indrayana <indra@indrayana.net>
2021-08-03 12:07:35 +0200

    ...also in README.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 If you're using `git` as your version control and want to work on non-mergeable object, like `Unity Scenes` or `Prefabs`, you can definitely use me to tell all of the team members that you're working on them!
 
-The channels in your Discord is meant to be seen as ***one*** project, so if you're working on multiple projects, you can have coordinated working without polluting the lock status of other channels.
+You can call me in an already existing channel (or dedicated) for your project. If you're working on multiple projects, you can call me in multiple channels without polluting the lock status of other projects! *p.s. I also keep the channel tidy!*
 
 Try using `!lock yourSceneName` to announce **I'M WORKING ON THIS :kissing_heart:**  
 When you're finished, use `!unlock yourSceneName` to say **I'M DONE WITH THIS :sunglasses:**
--- src/utility/about-text.js
+++ src/utility/about-text.js
@@ -3,7 +3,7 @@ const description = `I am a bot to help you avoid merge conflicts in (Game) Dev 
         
         If you're using \`git\` as your version control and want to work on non-mergeable object, like \`Unity Scenes\` or \`Prefabs\`, you can use me to tell all of the team members what you're working on!
 
-        You can call me in already existing channel for your project. If you're working on multiple projects, you can call me in multiple channels without polluting the lock status of other projects! *p.s. I also keep the channel tidy!*
+        You can call me in already existing channel (or dedicated) for your project. If you're working on multiple projects, you can call me in multiple channels without polluting the lock status of other projects! *p.s. I also keep the channel tidy!*
         
         Try using
         \`!lock yourSceneName\`
