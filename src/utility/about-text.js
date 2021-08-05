eeb2736f2e8d174f4c1cbff1dae34eb7b5079a24
Made Indrayana <indra@doubleshot-audio.com>
2021-08-05 15:29:28 +0200

    Updated about-text.js

--- src/utility/about-text.js
+++ src/utility/about-text.js
@@ -3,14 +3,15 @@ const description = `I am a bot to help you avoid merge conflicts in (Game) Dev 
         
         If you're using \`git\` as your version control and want to work on non-mergeable object, like \`Unity Scenes\` or \`Prefabs\`, you can use me to tell all of the team members what you're working on!
 
-        You can call me in already existing channel (or dedicated) for your project. If you're working on multiple projects, you can call me in multiple channels without polluting the lock status of other projects! *p.s. I also keep the channel tidy!*
+        You can call me in already existing channel (or a dedicated lock channel) for your project. If you're working on multiple projects, you can call me in multiple channels without affecting the lock status of other projects!
+        *p.s. I also keep the channel tidy!*
         
         Try using
-        \`!lock yourSceneName\`
+        \`!lock sceneName\` or \`!lock scene01 scene02\` 
         to announce **I'M WORKING ON THIS :nerd:**
 
         When you're finished, use
-        \`!unlock yourSceneName\`
+        \`!unlock\` or \`!unlock sceneName\` or even \`!unlock scene01 scene02 scene\`
         to say **I'M DONE WITH THIS :sunglasses:**
         
         Whenever you want to know who is working on what in a project, use
