1b990438c141b636c914d5b458fa6c481437b19e
Made Indrayana <indra@indrayana.net>
2021-08-09 12:27:52 +0200

    Bug fix
    - For some reason this code made problems because of ".DS_Store". Added fix for that

--- src/main.js
+++ src/main.js
@@ -14,10 +14,13 @@ client.commands = new Discord.Collection();
 const commandFolders = fs.readdirSync('src/commands');
 
 for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(`src/commands/${folder}`).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
+    if (folder === '.DS_Store') ;
+    else {
+        const commandFiles = fs.readdirSync(`src/commands/${folder}`).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./commands/${folder}/${file}`);
+            client.commands.set(command.name, command);
+        }
     }
 }
 
